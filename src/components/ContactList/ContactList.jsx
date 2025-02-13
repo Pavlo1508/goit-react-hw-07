41b2004ebfa02dc8262dc2d4dcdfc408e8d3de35
PKdev <130464415+Pavlo1508@users.noreply.github.com>
2025-02-14 00:14:46 +0100

    Deleted persist. Added selectors.js

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,16 +1,12 @@
 import { useSelector, useDispatch } from "react-redux";
 import { deleteContact } from "../../redux/contactsOps";
+import { selectFilteredContacts } from "../../redux/selectors";
 import ContactItem from "../Contact/Contact";
 import s from "./ContactList.module.css";
 
 const ContactList = () => {
   const dispatch = useDispatch();
-  const contacts = useSelector((state) => state.contacts.items);
-  const searchQuery = useSelector((state) => state.filters.name);
-
-  const filteredContacts = contacts.filter((contact) =>
-    contact.name.toLowerCase().includes(searchQuery.toLowerCase())
-  );
+  const filteredContacts = useSelector(selectFilteredContacts);
 
   const handleDelete = (id) => {
     dispatch(deleteContact(id));
